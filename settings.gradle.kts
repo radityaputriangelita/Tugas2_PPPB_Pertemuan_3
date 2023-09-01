2780d6d11aa5cb24e3c9abb3fbffabe10989f7c7
radityaputriangelita <radityaputriangelita@mail.ugm.ac.id>
2023-09-01 22:25:17 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tugas2_PPPB_Pertemuan_3"
+include(":app")
